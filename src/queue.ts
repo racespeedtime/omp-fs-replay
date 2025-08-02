66125eaf78d276b003d77916d68b7a7dd6e54500
dockfries <yucarl77@foxmail.com>
2025-08-02 13:44:39 +0800

    1

--- src/queue.ts
+++ src/queue.ts
@@ -1,4 +1,5 @@
 import { GameMode, Npc } from "@infernus/core";
+import { IRPC, PacketIdList } from "@infernus/raknet";
 
 const NPC_POOL_SIZE = 30;
 export const npcQueue = new Map<number, ReplayNpc>();
@@ -32,5 +33,20 @@ GameMode.onInit(({ next }) => {
 GameMode.onExit(({ next }) => {
   npcQueue.forEach((npcInst) => npcInst.destroy());
   npcQueue.clear();
+  return next();
+});
+
+// 拦截掉可能产生的npc心跳包移动数据，后续我们手动模拟npc数据不会进入到这里
+IRPC(PacketIdList.OnFootSync, ({ playerId, next }) => {
+  if (npcQueue.has(playerId)) {
+    return false;
+  }
+  return next();
+});
+
+IRPC(PacketIdList.DriverSync, ({ playerId, next }) => {
+  if (npcQueue.has(playerId)) {
+    return false;
+  }
   return next();
 });
